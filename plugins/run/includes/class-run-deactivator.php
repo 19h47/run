e7605886434db222ae44a64b8318a034f7345a0a
Jérémy Levron <jeremy@poigneedemainvirile.com>
2017-05-24 09:06:35 +0200

    Begin plugin for CPT run

---
+++ plugins/run/includes/class-run-deactivator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       http://www.19h47.fr
+ * @since      1.0.0
+ *
+ * @package    Run
+ * @subpackage Run/includes
+ */
+
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Run
+ * @subpackage Run/includes
+ * @author     Levron Jérémy <levronjeremy@19h47.fr>
+ */
+class Run_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
---
+++ plugins/run/run.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Run Post Type
+ *
+ * @link              http://www.19h47.fr
+ * @since             1.0.0
+ * @package           Run
+ *
+ * @wordpress-plugin
+ * Plugin Name:       	Run
+ * Plugin URI:        	http://stepper.19h47.fr/
+ * Description:       	Enables a run post type, taxonomy and metaboxes.
+ * Version:           	1.0.0
+ * Author:            	Levron Jérémy
+ * Author URI:        	http://www.19h47.fr
+ * License:           	GPL-2.0+
+ * License URI:       	http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       	run
+ * Domain Path:       	/languages
+ */
+
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-run.php';
+
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_run() {
+
+	$plugin = new Run();
+	$plugin->run();
+
+}
+
+
+// Run, Forrest, Run!
+run_run();
